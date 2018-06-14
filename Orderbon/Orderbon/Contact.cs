fb65320ec530bf4e08cd11deca03bbe2547a3c2e
Gino Derycke <deryckegino@gmail.com>
2018-06-14 13:03:14 +0200

    Temp

--- Orderbon/Orderbon/Contact.cs
+++ Orderbon/Orderbon/Contact.cs
@@ -1,33 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Text;
 using Xamarin.Forms;
 
 namespace Orderbon
 {
-    public class Contact
+    public class Contact : INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+
         public int Id { get; set; }
         public string Name { get; set; }
         public string Code { get; set; }
         public string Phone { get; set; }
         public string Group { get; set; }
-        /*
-         public Image Icon
+
+        private void OnPropertyChanged(string propertyName)
         {
-            get
-            {
-                
-                Image temp = new Image();
-                //temp.Width = "50";
-                //temp.Height = "50";
-                //temp.BackgroundColor = "yellow";
-
-
-                return temp;
-            }
-
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
         }
-        */
     }
 }
